b55854120b33f1a046921b71b15e9764a6209e9a
AFTABALAM75 <145189394+AFTABALAM75@users.noreply.github.com>
2024-02-17 21:35:55 +0530

    Add files via upload

---
+++ Python/first.py
@@ -0,0 +1,11 @@
+print("Hello World")
+a=10
+b=20
+#Sum of  two number
+print(a+b)
+#subtraction of  two number
+print(a-b)
+#multiplication of  two number
+print(a*b)
+#Division of  two number
+print(a/b)
